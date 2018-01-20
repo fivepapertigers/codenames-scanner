4966427a43ce69af674f4f21c3237c4d06a09f18
Jacob Griswold-Moran <jacob.griswold-moran@successacademies.org>
2018-01-20 10:58:46 -0500

    updates authorize-upload handler to use aws-helpers

--- packages/backend/src/handlers/authorize-upload.js
+++ packages/backend/src/handlers/authorize-upload.js
@@ -1,13 +1,14 @@
 import { UPLOAD_FOLDER } from "../config";
 
 import File from "../models/file";
+import { authorizeS3Post, authorizeS3Retrieval } from "../aws-helpers";
 import { apiProxyHandler } from "./util/lambda-handler";
 
 async function authorizeUpload () {
 
     const file = new File(UPLOAD_FOLDER);
-    const retrieve = await file.getRetrievalLink();
-    const save = await file.getSaveLink();
+    const retrieve = await authorizeS3Retrieval(file.fullPath());
+    const save = await authorizeS3Post(file.fullPath());
     return { retrieve, save };
 }
 
--- packages/backend/src/handlers/authorize-upload.test.js
+++ packages/backend/src/handlers/authorize-upload.test.js
@@ -4,20 +4,31 @@
 
 import File from "../models/file";
 import { handler } from "./authorize-upload";
+import { authorizeS3Post, authorizeS3Retrieval } from "../aws-helpers";
 
 jest.mock("../models/file",
     () => jest.genMockFromModule("../models/file"));
 
-const RETRIEVE_LINK = "http://retrieval";
-const SAVE_LINK = "http://save";
+jest.mock("../aws-helpers");
+
+const RETRIEVE_DATA = "http://retrieval";
+const SAVE_DATA = {"some": "field"};
+const FILE_PATH = "somecoll/someid";
+
+File.mockImplementation(() => ({
+    fullPath: () => FILE_PATH,
+}));
 
 describe("handler function", async () => {
 
     beforeEach(async () => {
-        File.mockImplementation(() => ({
-            getRetrievalLink: async () => RETRIEVE_LINK,
-            getSaveLink: async () => SAVE_LINK
-        }));
+        authorizeS3Post.mockReturnValue(SAVE_DATA);
+        authorizeS3Retrieval.mockReturnValue(RETRIEVE_DATA);
+    });
+
+    afterEach(() => {
+        authorizeS3Post.mockReset();
+        authorizeS3Retrieval.mockReset();
     });
 
 
@@ -27,10 +38,18 @@ describe("handler function", async () => {
         expect(File.mock.calls[0]).toEqual(["image-input"]);
     });
 
-    it("gets links", async () => {
+    it("returns authorizations", async () => {
         const { retrieve, save } = await handler();
-        expect(retrieve).toBe(RETRIEVE_LINK);
-        expect(save).toBe(SAVE_LINK);
+        expect(retrieve).toBe(RETRIEVE_DATA);
+        expect(save).toBe(SAVE_DATA);
+    });
+
+    it("authorizes via s3", async () => {
+        await handler();
+        expect(authorizeS3Retrieval.mock.calls).toHaveLength(1);
+        expect(authorizeS3Retrieval.mock.calls[0]).toEqual([FILE_PATH]);
+        expect(authorizeS3Post.mock.calls).toHaveLength(1);
+        expect(authorizeS3Post.mock.calls[0]).toEqual([FILE_PATH]);
     });
 
 });
